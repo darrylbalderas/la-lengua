72fb20b2574b2b348d3513826ffcda69b6bebff5
darrylbalderas <drrylbalderas@gmail.com>
2019-03-08 05:43:45 -0800

    Implement getting artist id
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- api/app.py
+++ api/app.py
@@ -1,5 +1,10 @@
 import requests
-from api.config import Config as conf
+
+
+class Config:
+    ACCESS_TOKEN = ""
+    ARTIST_TO_SEARCH = ["bad bunny", "yg", "nipsey hussle"]
+    BASE_URL = "https://api.genius.com/"
 
 
 def format_query(query):
@@ -11,16 +16,46 @@ def perform_response(url):
     """Perform HTTP GET REQUEST"""
     headers = {
         "Accept": "application/json",
-        "Authorization": "Bearer " + conf.ACCESS_TOKEN,
+        "Authorization": "Bearer " + Config.ACCESS_TOKEN,
     }
     return requests.get(url, headers=headers)
 
 
+def possible_artist_id(primary_artists):
+    """Get the artist id that shows up more often"""
+    if len(primary_artists) == 0:
+        return None
+    counts = {}
+    max_id = primary_artists[0]["id"]
+    counts[max_id] = 1
+    for primary_artist in primary_artists:
+        artist_id = primary_artist["id"]
+        if artist_id not in counts:
+            counts[artist_id] = 1
+        counts[artist_id] += 1
+        if counts[artist_id] > counts[max_id]:
+            max_id = artist_id
+    return max_id
+
+
 def get_artist_id(query):
-    """Extract Artist Id from search results for a particular artist"""
-    url = "{}search?q={}".format(conf.BASE_URL, format_query(query))
-    response = perform_response(url)
-    return response.json()
+    """Extract artist id from search results for a particular artist"""
+    url = "{}search?q={}&offset=10&limit=5".format(
+        Config.BASE_URL, format_query(query)
+    )
+    response = perform_response(url).json()
+    primary_artists = [
+        hit["result"]["primary_artist"] for hit in response["response"]["hits"]
+    ]
+    return possible_artist_id(primary_artists)
 
 
-ids = [get_artist_id(artist_name) for artist_name in conf.ARTIST_TO_SEARCH]
+def main():
+    ids = [
+        get_artist_id(artist_name) for artist_name in Config.ARTIST_TO_SEARCH
+    ]
+    print(ids)
+
+
+if __name__ == "__main__":
+    main()
--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -11,3 +11,24 @@ class TestApp(TestCase):
         self.assertEqual(
             app.format_query("   mumford and sons    "), "mumford%20and%20sons"
         )
+
+    def test_possible_artist_id(self):
+        primary_artists = [
+            {"id": 690350},
+            {"id": 1593971},
+            {"id": 123456},
+            {"id": 690350},
+            {"id": 690350},
+            {"id": 654321},
+        ]
+        self.assertEqual(app.possible_artist_id(primary_artists), 690350)
+        primary_artists = [
+            {"id": 123456},
+            {"id": 123456},
+            {"id": 123456},
+            {"id": 123456},
+            {"id": 690350},
+            {"id": 690350},
+        ]
+        self.assertEqual(app.possible_artist_id(primary_artists), 123456)
+        self.assertEqual(app.possible_artist_id([]), None)
