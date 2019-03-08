1d488ed39ab554c0cd178424ea43d4a9db1bb234
darrylbalderas <drrylbalderas@gmail.com>
2019-03-07 20:52:38 -0800

    Add pytest and black into project for formatting and testing
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- api/app.py
+++ api/app.py
@@ -1,25 +1,26 @@
 import requests
-from config import Config as conf
+from api.config import Config as conf
 
 
 def format_query(query):
     """Replaces spaces for %20"""
-    return '%20'.join(query.split())
+    return "%20".join(query.split())
 
 
 def perform_response(url):
     """Perform HTTP GET REQUEST"""
-    headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + conf.ACCESS_TOKEN}
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Bearer " + conf.ACCESS_TOKEN,
+    }
     return requests.get(url, headers=headers)
 
 
 def get_artist_id(query):
     """Extract Artist Id from search results for a particular artist"""
-    url = '{}search?q={}'.format(format_query(conf.BASE_URL, query))
+    url = "{}search?q={}".format(conf.BASE_URL, format_query(query))
     response = perform_response(url)
     return response.json()
 
 
 ids = [get_artist_id(artist_name) for artist_name in conf.ARTIST_TO_SEARCH]
-
-
--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    ACCESS_TOKEN = ''
-    ARTIST_TO_SEARCH = ['bad bunny', 'yg', 'nipsey hussle']
-    BASE_URL = 'https://api.genius.com/'
